3bc6e8e5d64358d8907a43b27240a6c948012f0c
lazy <lazy@dnion.com>
2020-09-27 00:24:01 +0800

    test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World-lazy");
+		get("/hello", (req, res) -> "Hello World-lazy-good");
 	}
 }
